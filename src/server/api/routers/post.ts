00fe995951f6159fab4e0dcb80fe763a6a7558a3
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2024-10-05 21:42:18 +0530

    feat: added clerk

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -11,16 +11,6 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
   getLatest: publicProcedure.query(async ({ ctx }) => {
     const post = await ctx.db.post.findFirst({
       orderBy: { createdAt: "desc" },
